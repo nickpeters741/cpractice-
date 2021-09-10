e90dd7340a2450bc26215c2323b9b220d236f9c4
nickpeters741 <npeters741@gmail.com>
2021-09-10 05:46:57 -0700

    fun practice

---
+++ circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int radius;
+	
+	scanf("%i", &radius);
+	printf("%i\n", radius);
+	return 0;
+
+
+
+}
---
+++ motion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    x = 30;
+
+    int y = x / 2;
+    
+    printf("hey you nailed the math!: %i\nThe value of x is %i\n" ,y ,x );
+    return 0;
+}
